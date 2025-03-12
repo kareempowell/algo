fb66b1d33dcfbed148c9c8a27461d2a9c19bee03
Kareem Powell <105847443+kareempowell@users.noreply.github.com>
2025-03-11 22:41:32 -0700

    Update algo.py

--- algo/algo.py
+++ algo/algo.py
@@ -197,10 +197,10 @@ class algo:
     forex_pattern = re.compile(r"^[A-Z]{3}/[A-Z]{3}$")
     return [instrument[1] for instrument in self.instruments_data if forex_pattern.match(instrument[0])]
        
-  def select_instrument(self, instruments, startdate, enddate):
-    #selecting instruments N.B. 1st line previously data = oanda.get_history
+  def select_instrument(self, forex_instruments, startdate, enddate):
+    #selecting forex_instruments N.B. 1st line previously data = oanda.get_history
     datalist=[]
-    for instrument in instruments:
+    for instrument in forex_instruments:
        data = self.api.get_history(
           instrument=instrument,
           start=startdate,
